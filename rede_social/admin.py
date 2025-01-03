4efde3f6a1bac8e626d58b9b0298aa4cdf07713b
Pirraio <g.teixeira7x@gmail.com>
2025-01-02 23:51:16 -0300

    Add model User

--- rede_social/admin.py
+++ rede_social/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from rede_social.models import Usuario
 
-# Register your models here.
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'name', 'email']
+    list_display_links = ['id', 'username']
+    list_per_page = 30
+    search_fields = ('username','email',)
+    ordering = ('id',)
+
+admin.site.register(Usuario, UsuarioAdmin)
--- rede_social/models.py
+++ rede_social/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    username = models.CharField(max_length=16, unique=True)
+    email = models.EmailField(max_length=30, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class Postagens(models.Model):
+    pass
+
+class Comentarios(models.Model):
+    pass
