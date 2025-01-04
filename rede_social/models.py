efa654ebd188019e0ffbaff41c72ffd3027b7cb6
Pirraio <g.teixeira7x@gmail.com>
2025-01-03 23:16:03 -0300

    Definição dos models

--- rede_social/models.py
+++ rede_social/models.py
@@ -1,15 +1,26 @@
 from django.db import models
+#from datetime import datetime
 
 class Usuario(models.Model):
-    username = models.CharField(max_length=16, unique=True)
-    email = models.EmailField(max_length=30, unique=True)
+    username = models.CharField(max_length=16, unique=True, blank=False)
+    email = models.EmailField(max_length=30, unique=True, blank=False)
+    password = models.CharField(max_length=100, blank=False, null=False)
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.username
 
-class Postagens(models.Model):
-    pass
+class Postagem(models.Model):
+    texto = models.TextField(null=False, blank=False, max_length=500)
+    usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+    data_hora = models.DateTimeField(auto_now_add=True, blank=False)
 
-class Comentarios(models.Model):
-    pass
+    def __str__(self):
+        return self.texto
+
+class Comentario(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+    postagem = models.ForeignKey(Postagem, on_delete = models.CASCADE)
+    comentario = models.TextField(max_length=100, null=False, blank=False)
+    data_hora = models.DateTimeField(auto_now_add=True, blank=False, null=True)
+
