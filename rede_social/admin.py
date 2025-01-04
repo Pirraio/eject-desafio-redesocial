b37c60a5668f124fb643a47ea5011bba73ca16c9
Pirraio <g.teixeira7x@gmail.com>
2025-01-03 23:16:53 -0300

    Configuração do admin

--- rede_social/admin.py
+++ rede_social/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from rede_social.models import Usuario
+from rede_social.models import Usuario, Postagem, Comentario
 
-class UsuarioAdmin(admin.ModelAdmin):
+class UsuariosAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'name', 'email']
     list_display_links = ['id', 'username']
     list_per_page = 30
     search_fields = ('username','email',)
     ordering = ('id',)
 
-admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Usuario, UsuariosAdmin)
+
+class PostagensAdmin(admin.ModelAdmin):
+    list_display = ['id', 'texto', 'data_hora']
+    list_display_links = ['id']
+    search_fields = ('texto',)
+
+admin.site.register(Postagem,PostagensAdmin)
+
+class ComentariosAdmin(admin.ModelAdmin):
+    list_display = ['id', 'usuario', 'postagem', 'comentario', 'data_hora']
+    list_display_links = ['id', 'usuario', 'postagem']
+    search_fields = ('usuario', 'postagem', 'comentario',)
+    
+admin.site.register(Comentario,ComentariosAdmin)
